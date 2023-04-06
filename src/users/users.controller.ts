26a18d59a8ed704bf35450a57c7d87da44c3387d
karatayburakk <54941630+karatayburakk@users.noreply.github.com>
2023-04-06 04:10:02 +0300

    CRUD operation on user entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,53 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Post('signup')
-  createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+  signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.signup(createUserDto);
+  }
+
+  @Get(':id')
+  async findOneById(@Param('id') id: number): Promise<User> {
+    console.log(typeof id);
+    const user = await this.usersService.findOneById(id);
+
+    if (!user) throw new NotFoundException('User Not Found');
+
+    return user;
+  }
+
+  @Get()
+  findAllByEmail(@Query('email') email: string): Promise<User[]> {
+    return this.usersService.findAllByEmail(email);
+  }
+
+  @Delete(':id')
+  removeOneById(@Param('id') id: number): Promise<User> {
+    return this.usersService.removeOneByID(id);
+  }
+
+  @Patch(':id')
+  updateOneById(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.usersService.updateOneById(id, updateUserDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepo: Repository<User>,
+  ) {}
+
+  signup(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepo.create(createUserDto);
+
+    return this.usersRepo.save(user);
+  }
+
+  findOneById(id: number): Promise<User> {
+    return this.usersRepo.findOne({ where: { id } });
+  }
+
+  findAllByEmail(email: string): Promise<User[]> {
+    return this.usersRepo.find({ where: { email } });
+  }
+
+  async updateOneById(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.findOneById(id);
+
+    if (!user) throw new NotFoundException('User Not Found');
+
+    Object.assign(user, updateUserDto);
+
+    return this.usersRepo.save(user);
+  }
+
+  async removeOneByID(id: number): Promise<User> {
+    const user = await this.findOneById(id);
+
+    if (!user) throw new NotFoundException('User Not Found');
+
+    return this.usersRepo.remove(user);
+  }
+}
