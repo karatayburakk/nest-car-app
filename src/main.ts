fa0d0d83511896a91a09ec947a414c99a255b11a
karatayburakk <54941630+karatayburakk@users.noreply.github.com>
2023-04-07 17:40:51 +0300

    End to end testing

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import CookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(
-    CookieSession({
-      keys: ['asdasdas'],
-    }),
-  );
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
 
   await app.listen(3000);
 }
