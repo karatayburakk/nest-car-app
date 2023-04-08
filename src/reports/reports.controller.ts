821ed621acc465650d02ae2317b2a4e7537bf603
karatayburakk <54941630+karatayburakk@users.noreply.github.com>
2023-04-08 15:21:47 +0300

    basic permission system

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
 import { ReportsService } from './reports.service';
@@ -7,10 +16,18 @@ import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(typeof query.year);
+  }
 
   @UseGuards(AuthGuard)
   @Serialize(ReportDto)
@@ -20,5 +37,14 @@ export class ReportsController {
     @CurrentUser() user: User,
   ): Promise<Report> {
     return this.reportsService.create(createReportDto, user);
+  }
+
+  @Patch(':id')
+  @UseGuards(AdminGuard)
+  approveReport(
+    @Param('id') id: number,
+    @Body() approveReportDto: ApproveReportDto,
+  ) {
+    return this.reportsService.changeApproval(id, approveReportDto);
   }
 }
